7d763753eed713313b8d68f2587256616ab0457a
IIRoan <github.com.q6c0y@lunary.roan.zip>
2025-04-02 16:53:44 +0200

    t

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,11 @@ export default defineConfig({
     port: 8080,
     host: true, // listen on all addresses
     strictPort: true, // fail if port is already in use
+    allowedHosts: ['app-010c5f04-282e-4860-83c1-f688be97c218.cleverapps.io', '.cleverapps.io']
   },
   server: {
     host: '0.0.0.0',
     port: 8080,
+    allowedHosts: ['app-010c5f04-282e-4860-83c1-f688be97c218.cleverapps.io', '.cleverapps.io']
   }
 })
